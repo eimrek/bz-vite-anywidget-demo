8432ed0769d452d96b9a636c97151022dcf473ed
Kristjan Eimre <kristjaneimre@gmail.com>
2024-03-10 17:00:50 +0200

    run prettier on all files

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import './style.css'
+import "./style.css";
 
-import {createBZVisualizer} from "./src/create-bzvisualizer.js"
+import { createBZVisualizer } from "./src/create-bzvisualizer.js";
 
 import jsondata1 from "./src/example_data/mc3d-10.json";
 import jsondata2 from "./src/example_data/mc3d-10016.json";
 
-createBZVisualizer("bzvis-1", jsondata1)
+createBZVisualizer("bzvis-1", jsondata1);
 
-createBZVisualizer("bzvis-2", jsondata2)
+createBZVisualizer("bzvis-2", jsondata2);
